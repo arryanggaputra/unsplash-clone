fb28c309572e808dbcc33c155409e6f5ac3fdc6d
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2021-09-20 10:05:00 +0700

    :lipstick: use home page and show default star-wars collections

--- src/modules/main/presentation/home/index.tsx
+++ src/modules/main/presentation/home/index.tsx
@@ -1,11 +1,36 @@
-import React from "react";
-import SearchBar from "modules/main/components/SearchBar";
+import React, { useMemo, useState } from "react";
+import { getPhotos } from "modules/infrastructure/http/collections";
+import { CollectionsPhotosParams } from "modules/infrastructure/types";
+import useSWR from "swr";
+import ImageThumbnail from "modules/main/components/ImageThumbnail";
+import Layout from "modules/main/components/Layout";
+
+const collectionId = "2423569";
 
 const Home: React.FC<{}> = () => {
+  const [page, setPage] = useState(1);
+
+  const collectionParameter: CollectionsPhotosParams = useMemo(
+    () => ({
+      collectionId,
+      page,
+      per_page: 20,
+    }),
+    []
+  );
+
+  const { data: collectionImages } = useSWR([collectionParameter], getPhotos);
+
   return (
-    <div className="max-w-screen-lg mx-auto pt-10">
-      <SearchBar />
-    </div>
+    <Layout>
+      <div className="mx-auto">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-4 ">
+          {collectionImages?.data?.map((item) => {
+            return <ImageThumbnail key={item.id} {...item} />;
+          })}
+        </div>
+      </div>
+    </Layout>
   );
 };
 
