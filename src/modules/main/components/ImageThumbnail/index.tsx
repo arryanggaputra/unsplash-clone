d3c1f722966bcfeb9d849990a0c2579606cb352c
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2021-09-20 14:25:07 +0700

    :wheelchair: add image preloader using blur_hash

--- src/modules/main/components/ImageThumbnail/index.tsx
+++ src/modules/main/components/ImageThumbnail/index.tsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useCallback, useMemo, useState } from "react";
+import { Blurhash } from "react-blurhash";
 import { Basic } from "unsplash-js/dist/methods/photos/types";
 
 const ImageThumbnail: React.FC<Basic> = (props) => {
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  const afterLoad = useCallback(() => {
+    setTimeout(() => {
+      setIsImageLoaded(true);
+    }, 1000);
+  }, []);
+
+  const placeholder = useMemo(() => {
+    if (isImageLoaded) return <></>;
+    return (
+      <Blurhash
+        hash={props.blur_hash || ""}
+        width={"100%"}
+        height={"100%"}
+        resolutionX={32}
+        resolutionY={32}
+        punch={1}
+      />
+    );
+  }, [isImageLoaded, props.blur_hash]);
+
   return (
     <div className="h-40 overflow-hidden transition-transform hover:shadow-md rounded-md group cursor-pointer hover:scale-[120%]">
+      {placeholder}
       <img
         src={props.urls.small}
+        loading="lazy"
         alt={props.alt_description || ""}
-        className="object-cover h-full w-full  rounded-md "
+        onLoad={afterLoad}
+        className={`object-cover h-full w-full rounded-md ${
+          !isImageLoaded ? "hidden" : ""
+        }`}
       />
     </div>
   );
