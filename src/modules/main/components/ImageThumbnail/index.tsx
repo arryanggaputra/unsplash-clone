483a633202ac6a5e442b4b1df7427c3e1416d8a2
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2021-09-20 16:52:06 +0700

    :recycle: move lazyload loading into component

--- src/modules/main/components/ImageThumbnail/index.tsx
+++ src/modules/main/components/ImageThumbnail/index.tsx
@@ -1,51 +1,30 @@
-import useOnScreen from "hooks/useOnScreen";
-import React, { useCallback, useMemo, useRef, useState } from "react";
-import { Blurhash } from "react-blurhash";
+import useStore from "modules/infrastructure/store";
+import React, { useCallback } from "react";
+
 import { Basic } from "unsplash-js/dist/methods/photos/types";
+import ImagePreloader from "../ImageLazyLoad";
 
-const ImageThumbnail: React.FC<Basic> = (props) => {
-  const [isImageLoaded, setIsImageLoaded] = useState(false);
+interface IImageThumbnail {
+  onClick?: (photo: Basic) => void;
+}
 
-  const imageWrapper = useRef<HTMLDivElement>(null);
-  const isImageVisibleOnScreen = useOnScreen(imageWrapper, "0px");
+const ImageThumbnail: React.FC<Basic & IImageThumbnail> = (props) => {
+  const { setSelectedPhoto } = useStore((state) => state);
 
-  const afterLoad = useCallback(() => {
-    setTimeout(() => {
-      setIsImageLoaded(true);
-    }, 1000);
-  }, []);
-
-  const placeholder = useMemo(() => {
-    if (isImageLoaded) return <></>;
-    return (
-      <Blurhash
-        hash={props.blur_hash || ""}
-        width={"100%"}
-        height={"100%"}
-        resolutionX={32}
-        resolutionY={32}
-        punch={1}
-      />
-    );
-  }, [isImageLoaded, props.blur_hash]);
+  const onClick = useCallback(() => {
+    setSelectedPhoto(props);
+  }, [props]);
 
   return (
     <div
-      ref={imageWrapper}
+      onClick={onClick}
       className="h-40 overflow-hidden transition-transform hover:shadow-md rounded-md group cursor-pointer hover:scale-[120%]"
     >
-      {placeholder}
-      {isImageVisibleOnScreen && (
-        <img
-          src={props.urls.small}
-          loading="lazy"
-          alt={props.alt_description || ""}
-          onLoad={afterLoad}
-          className={`object-cover h-full w-full rounded-md ${
-            !isImageLoaded ? "hidden" : ""
-          }`}
-        />
-      )}
+      <ImagePreloader
+        src={props.urls.small}
+        alt={props.alt_description || ""}
+        blurHash={props.blur_hash || ""}
+      />
     </div>
   );
 };
