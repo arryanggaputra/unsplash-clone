0a697f3e548432d72a0dc760e1745ec7da9e1915
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2021-09-20 10:04:26 +0700

    :lipstick: use react-router to switch between page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Search from "modules/main/presentation/search";
 import Home from "modules/main/presentation/home";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/search" component={Search} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
